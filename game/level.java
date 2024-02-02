eea8cdf56e37a429ab4cd0274a32462140a7df84
Cristi <raducea.matei2005@gmail.com>
2024-02-02 05:26:30 +0200

    sa

--- game/level.java
+++ game/level.java
@@ -14,7 +14,6 @@ public abstract class level extends World
     int mapHeight;
     int mapWidth;
     private vector2[][] tileCoordinates;
-    List<bricks> tilesToRemove = new ArrayList<>();
     shiro player = null;
     public level()
     {
@@ -52,9 +51,36 @@ public abstract class level extends World
     public void act(){
         player = (shiro) getObjects(shiro.class).get(0);
         if (player.getX()>getWidth()/2){
-            player.setLocation(player.getX()-player.speed, player.getY());
-            offset=-player.speed;
-            for (int i=0; i<mapHeight; i++){
+            moveCamera(1);
+        }
+        else if (player.getX()<getWidth()/2){
+            moveCamera(-1);
+        }
+    }
+    private static int getHeightFromJsonFile(File jsonFile) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+
+            // Extract the value of "height" from the root JSON object
+            int height = jsonNode.get("height").asInt();
+
+            return height;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1; // Handle the error appropriately in your application
+        }
+    }
+    private boolean locationOnScreen(vector2 location){
+        if(location.x>player.getX()-getWidth()/2&&location.x<player.getX()+getWidth()/2){
+            return true;
+        }
+        return false;
+    }
+    private void moveCamera(int direction){//1 right -1 left
+        player.setLocation(player.getX()-player.speed*direction, player.getY());
+        offset=-player.speed*direction;
+        for (int i=0; i<mapHeight; i++){
                 for (int j=0; j<mapWidth; j++)
                 {
                     int kind = "012345678".indexOf(""+map.charAt(i*mapWidth+j));
@@ -82,59 +108,6 @@ public abstract class level extends World
                     }
                 }
             }
-        }
-        else if (player.getX()<getWidth()/2){
-            player.setLocation(player.getX()+player.speed, player.getY());
-            offset=player.speed;
-            for (int i=0; i<mapHeight; i++){
-                for (int j=0; j<mapWidth; j++)
-                {
-                    int kind = "012345678".indexOf(""+map.charAt(i*mapWidth+j));
-                    if (kind < 0) continue;
-                    Actor actor = null;
-                    
-                    int tileX = 16 + j * 32;
-                    int tileY = 16 + i * 32;
-                    tileCoordinates[i][j].x+=offset;
-                    if (kind == 1) actor = new bricks();
-                    if(actor!=null){
-                        List<Actor> objectsAtLocation = getObjectsAt(tileCoordinates[i][j].x, tileCoordinates[i][j].y  , (Class<Actor>) actor.getClass());
-                        if(!objectsAtLocation.isEmpty()&&!locationOnScreen(tileCoordinates[i][j])){//it exist but not on screen
-                            bricks singleBrick = (bricks) objectsAtLocation.get(0);
-                            removeObject(singleBrick);
-                        }
-                        else if(!objectsAtLocation.isEmpty()&&locationOnScreen(tileCoordinates[i][j])){//if tile exists and on screen
-                            bricks singleBrick = (bricks) objectsAtLocation.get(0);
-                            singleBrick.setLocation(tileCoordinates[i][j].x, tileCoordinates[i][j].y); 
-                        }
-                        else if(objectsAtLocation.isEmpty()&&locationOnScreen(tileCoordinates[i][j])){//tile does not exist and is on screen then instantiate
-                            addObject(actor, tileCoordinates[i][j].x, tileCoordinates[i][j].y); 
-                        }
-                        //if tile exist but is not on screen
-                    }
-                }
-            }
-        }
-    }
-    private static int getHeightFromJsonFile(File jsonFile) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(jsonFile);
-
-            // Extract the value of "height" from the root JSON object
-            int height = jsonNode.get("height").asInt();
-
-            return height;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return -1; // Handle the error appropriately in your application
-        }
-    }
-    private boolean locationOnScreen(vector2 location){
-        if(location.x>player.getX()-getWidth()/2&&location.x<player.getX()+getWidth()/2){
-            return true;
-        }
-        return false;
     }
 
     public void setFields() {}
