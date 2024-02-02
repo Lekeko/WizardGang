0babb133ff0fd59cb3834f9ba5ae3ff160bebd70
Cristi <raducea.matei2005@gmail.com>
2024-02-02 16:05:44 +0200

    qwe

--- game/lvl1.java
+++ game/lvl1.java
@@ -7,30 +7,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 
 public class lvl1 extends level
 {
-    public void setFields()
-    {
-        File jsonFile = new File("untitled.json");
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(jsonFile);
-            //extract the "width" and "height" values from the root JSON object
-            mapWidth = jsonNode.get("width").asInt();
-            mapHeight = jsonNode.get("height").asInt();
-
-            //extract the "layers" array from the root JSON object
-            JsonNode layersNode = jsonNode.get("layers");
-            //extract the "data" array from the first layer in the "layers" array
-            JsonNode dataNode = layersNode.get(0).get("data");
-            //extract and concatenate the numbers from the "data" array
-            StringBuilder numbersStringBuilder = new StringBuilder();
-            for (JsonNode numberNode : dataNode) {
-                numbersStringBuilder.append(numberNode.asText());
-            }
-            // Convert the "data" array to a String
-            map = numbersStringBuilder.toString();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void setMap(){
+        jsonFile = new File("untitled.json");
     }//25,25 map will fit 800 800
 }
--- game/lvl2.java
+++ game/lvl2.java
@@ -7,52 +7,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 
 public class lvl2 extends level
 {
-    public void setFields()
-    {
-        File jsonFile = new File("untitled.json");
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(jsonFile);
-            // Extract the "width" and "height" values from the root JSON object
-            mapWidth = jsonNode.get("width").asInt();
-            mapHeight = jsonNode.get("height").asInt();
-
-            // Extract the "layers" array from the root JSON object
-            JsonNode layersNode = jsonNode.get("layers");
-            // Extract the "data" array from the first layer in the "layers" array
-            JsonNode dataNode = layersNode.get(0).get("data");
-
-            // Convert the "data" array to a String
-            map = dataNode.toString();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Handle the error appropriately in your application
-        }
-        /*map = new String[] { "                     pspppppppppppppppppppp",
-                             "d m           c    m     ",
-                             "ppp            pppppppppp",
-                             "                         ",
-                             "                  k      p",
-                             "     pp                  ",
-                             "                        p",
-                             "             pppppppppp  ",
-                             "                        p",
-                             "   f    pp               ",
-                             "               m                 p",
-                             "    m                    ",
-                             "             pppp     m                  p",
-                             "            pp           ",
-                             "    m                m  p",
-                             "         w        w                ppppp",
-                             "      p                 p       p",
-                             "     pp   p                 p",
-                             "    pp    p          pp p  p",
-                             "   pp     p     pppp      p",
-                             "p  p      p             pp",
-                             "pppppppppppp  ppppppppppp",
-                             "                        p",
-                             "                         ",
-                             "ppppppppppppppppppppppppppppppppppppppppppp",};*/
-    }//25,25 map will fit
+    public void setMap(){
+        jsonFile = new File("untitled.json");
+    }
 }
