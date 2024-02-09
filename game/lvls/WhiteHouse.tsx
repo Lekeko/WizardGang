8ad16223029b0a67bc2e24c3921f8e24840e1f92
Cristi <raducea.matei2005@gmail.com>
2024-02-10 01:46:15 +0200

    :)

---
+++ game/lvls/WhiteHouse.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="WhiteHouse" tilewidth="512" tileheight="128" tilecount="4" columns="2">
+ <image source="../images/casaAlba.png" width="1024" height="256"/>
+</tileset>
